392c66900db49178d396738a66281b14ac9df02e
Sannidhya Shukla <shuklasannidhya@gmail.com>
2013-08-02 03:57:34 +0000

    Create ec.js

---
+++ ec.js
@@ -0,0 +1,43 @@
+var Subshell, ssn;
+
+ssn = "spdfghijklmnoqrtuvwxyz";
+Subshell = {
+    n: 1,
+    s: 's',
+    maxE: 2,
+    advance: function() {
+        var nN, nS;
+        
+        if (this.s === 's') {
+            nN = Math.ceil(this.n / 2) + 1;
+            nS = ssn.charAt(nN - (this.n % 2) - 1);
+        } else {
+            nN = this.n + 1;
+            nS = ssn.charAt(ssn.indexOf(this.s) - 1);
+        }
+        this.n = nN;
+        this.s = nS;
+        this.maxE = 2 * (2 * ssn.indexOf(nS) + 1);
+    }
+};
+
+function getEC(aN) {
+    var cSubshell, out;
+    
+    cSubshell = Object.create(Subshell);
+    out = '';
+    while (aN > 0) {
+        out += cSubshell.n + cSubshell.s;
+        if (aN - cSubshell.n > 0) {
+            out += cSubshell.maxE;
+        } else {
+            out += aN;
+        }
+        aN -= cSubshell.maxE;
+        cSubshell.advance();
+        if (aN > 0) {
+            out += ', ';
+        }
+    }
+    return out;
+}
